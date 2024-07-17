18ca9e6b50ce2c2db65a7a4b7bbbd1cb72c2193b
Luke Fox <foxlc@mail.uc.edu>
2024-07-17 14:15:30 -0400

    Added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,21 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+
+const Content: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
+
+}
 
 function App() {
-  const [version, setVersion] = useState('' as string)
-
-  function getVersion() {
-    fetch('http://localhost:3001')
-      .then(response => response.json())
-      .then(data => setVersion(data))
-      .then(data => console.log(data))
-      .then(() => console.log(version)), {mode: 'no-cors'}
-
-  }
 
   return (
-    <div className="flex flex-col items-center justify-between w-full h-full">
-      <p className="">Store?</p>
-      <button className="px-4 py-2 text-white bg-blue-500 rounded-md" onClick={getVersion}>Get Version</button>
-      <div>{version}</div>
-    </div>
+    <Content />
   )
 }
 
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
+const Home: React.FC = () => {
+
+    const [version, setVersion] = useState('' as string)
+
+  function getVersion() {
+    fetch('http://localhost:3001')
+      .then(response => response.json())
+      .then(data => setVersion(data))
+      .then(data => console.log(data))
+      .then(() => console.log(version)), {mode: 'no-cors'}
+
+  }
+    return (
+        <>
+        <div className="flex flex-col items-center justify-between w-full h-full">
+      <p className="">Store?</p>
+      <button className="px-4 py-2 text-white bg-blue-500 rounded-md" onClick={getVersion}>Get Version</button>
+      <div>{version}</div>
+    </div>
+        </>
+        
+    )
+}
+
+export default Home;
